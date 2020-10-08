f9fc54c7f9aa81a0882acd8a4cd2cdbb03f0abc3
ArtZiom <artziom@gmail.com>
2020-10-08 16:02:38 +0200

    Test connection to database

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php
+$servername = "localhost";
+$username = "root";
+$password = "secret";
+$dbname = "test";
+
+try {
+    $dbh = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    echo "Connected successfully<br>";
+} catch (PDOException $e) {
+    print "Error!: " . $e->getMessage() . "<br/>";
+    die();
+}
+
 echo "Hello World!";
