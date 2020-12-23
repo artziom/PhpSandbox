0546c3d840e15633e824731f0435cf80b0ff2459
ArtZiom <artziom@gmail.com>
2020-12-24 00:08:04 +0100

    Add redis-commander container

--- index.php
+++ index.php
@@ -26,10 +26,13 @@ $redis->hSet("redis:person", 'name', $p1->getName());
 $redis->hSet("redis:person", 'age', $p1->getAge());
 var_dump($redis->hGetAll("redis:person"));
 var_dump($redis->hGet("redis:person", "name"));
+var_dump($redis->info("memory"));
+var_dump($redis->slowLog("GET"));
 
 $memcached = new Memcached();
 $memcached->addServer("memcached", 11211);
 $memcached->set("memcached:person", $p1);
 var_dump($memcached->get("memcached:person"));
+var_dump($memcached->getStats());
 
 $log->warning('Closing App');
