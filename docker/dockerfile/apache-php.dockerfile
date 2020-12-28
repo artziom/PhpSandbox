838e5c9a6a47726de8bdd5e200ffa96844cba111
ArtZiom <artur.ziomek@triverna.pl>
2020-12-28 11:06:10 +0100

    Clear project files and add mysql queries for example

--- docker/dockerfile/apache-php.dockerfile
+++ docker/dockerfile/apache-php.dockerfile
@@ -14,6 +14,7 @@ RUN pecl install xdebug-3.0.1 \
 	&& docker-php-ext-enable redis xdebug memcached \
     && docker-php-ext-install pdo_mysql zip
 
+# Install Blackfire agent
 RUN version=$(php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;") \
     && curl -A "Docker" -o /tmp/blackfire-probe.tar.gz -D - -L -s https://blackfire.io/api/v1/releases/probe/php/linux/amd64/$version \
     && mkdir -p /tmp/blackfire \
@@ -21,9 +22,6 @@ RUN version=$(php -r "echo PHP_MAJOR_VERSION.PHP_MINOR_VERSION;") \
     && mv /tmp/blackfire/blackfire-*.so $(php -r "echo ini_get ('extension_dir');")/blackfire.so \
     && printf "extension=blackfire.so\nblackfire.agent_socket=tcp://blackfire:8707\n" > $PHP_INI_DIR/conf.d/blackfire.ini \
     && rm -rf /tmp/blackfire /tmp/blackfire-probe.tar.gz
-
-# Please note that the Blackfire Probe is dependent on the session module.
-# If it isn't present in your install, you will need to enable it yourself.
 
 # Set php.ini file
 RUN mv "$PHP_INI_DIR/php.ini-development" "$PHP_INI_DIR/php.ini" \
--- index.php
+++ index.php
@@ -5,6 +5,10 @@ require __DIR__ . '/vendor/autoload.php';
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
+$log = new Logger("PhpSandbox");
+$log->pushHandler(new StreamHandler('var/log/app.log', Logger::WARNING));
+$log->warning('Starting App');
+
 try {
     $dbh = new PDO("mysql:host=" . getenv("MYSQL_HOST") . ";dbname=" . getenv("MYSQL_DBNAME") . "", getenv("MYSQL_USERNAME"), getenv("MYSQL_PASSWORD"));
     echo "Connected successfully<br>";
@@ -13,11 +17,20 @@ try {
     die();
 }
 
-$log = new Logger("PhpSandbox");
-$log->pushHandler(new StreamHandler('var/log/app.log', Logger::WARNING));
-$log->warning('Starting App');
+$query = $dbh->prepare("CREATE TABLE IF NOT EXISTS person (id int, name varchar(255), age int)");
+$query->execute();
 
 $p1 = new Person("Artur", 10);
+
+$query = $dbh->prepare("INSERT INTO person (name, age) VALUE (:name, :age)");
+$query->execute([
+    ":name" => $p1->getName(),
+    ":age" => $p1->getAge()
+]);
+$query = $dbh->prepare("SELECT * FROM person");
+$query->execute();
+$results = $query->fetchAll();
+var_dump($results);
 
 $redis = new Redis();
 $redis->connect("redis");
