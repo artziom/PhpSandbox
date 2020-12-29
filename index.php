fef31ab8cf4704427a9d6018bb55d4752abb7267
ArtZiom <artur.ziomek@triverna.pl>
2020-12-29 14:50:44 +0100

    Testing Psalm

--- index.php
+++ index.php
@@ -49,3 +49,17 @@ var_dump($memcached->get("memcached:person"));
 var_dump($memcached->getStats());
 
 $log->warning('Closing App');
+
+/**
+ * @return array<string>
+ */
+function takesAnInt(int $i) {
+    return [$i, "hello"];
+}
+
+$data = ["some text", 5];
+takesAnInt($data[0]);
+
+$condition = rand(0, 5);
+if ($condition) {
+} elseif ($condition) {}
