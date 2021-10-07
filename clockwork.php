3bb91fd7e77b9f9b1e88fde9c4efdb4de8cb991e
ArtZiom <artziom@gmail.com>
2021-10-07 15:53:51 +0200

    .

--- clockwork.php
+++ clockwork.php
@@ -2,5 +2,9 @@
 
 require "./vendor/autoload.php";
 
-$clockwork = Clockwork\Support\Vanilla\Clockwork::init([ 'api' => '/clockwork.php?request=' ]);
+$clockwork = Clockwork\Support\Vanilla\Clockwork::init([
+    'api' => '/clockwork.php?request=',
+    'register_helpers' => true,
+    'storage_files_path' => __DIR__ . '/clockwork'
+]);
 $clockwork->returnMetadata();
--- index.php
+++ index.php
@@ -3,7 +3,8 @@ require "./vendor/autoload.php";
 
 $clockwork = Clockwork\Support\Vanilla\Clockwork::init([
     'api' => '/clockwork.php?request=',
-    'register_helpers' => true
+    'register_helpers' => true,
+    'storage_files_path' => __DIR__ . '/clockwork'
 ]);
 
 $example = new ClockworkExample();
