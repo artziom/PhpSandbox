2ad719c77748924d97fe5a3d7c123df0683a331c
ArtZiom <artziom@gmail.com>
2021-11-30 13:05:06 +0100

    Add project files from Part 2

--- application/src/Entity/CheeseListing.php
+++ application/src/Entity/CheeseListing.php
@@ -9,71 +9,80 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
 use Carbon\Carbon;
-use DateTimeImmutable;
-use App\Repository\CheeseListingRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Serializer\Annotation\SerializedName;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ORM\Entity(repositoryClass=CheeseListingRepository::class)
+ * @ApiResource(
+ *     collectionOperations={"get", "post"},
+ *     itemOperations={
+ *          "get"={
+ *              "normalization_context"={"groups"={"cheese_listing:read", "cheese_listing:item:get"}},
+ *          },
+ *          "put"
+ *     },
+ *     shortName="cheeses",
+ *     normalizationContext={"groups"={"cheese_listing:read"}, "swagger_definition_name"="Read"},
+ *     denormalizationContext={"groups"={"cheese_listing:write"}, "swagger_definition_name"="Write"},
+ *     attributes={
+ *          "pagination_items_per_page"=10,
+ *          "formats"={"jsonld", "json", "html", "jsonhal", "csv"={"text/csv"}}
+ *     }
+ * )
+ * @ApiFilter(BooleanFilter::class, properties={"isPublished"})
+ * @ApiFilter(SearchFilter::class, properties={
+ *     "title": "partial",
+ *     "description": "partial",
+ *     "owner": "exact",
+ *     "owner.username": "partial"
+ * })
+ * @ApiFilter(RangeFilter::class, properties={"price"})
+ * @ApiFilter(PropertyFilter::class)
+ * @ORM\Entity(repositoryClass="App\Repository\CheeseListingRepository")
  */
-#[ApiResource(
-    collectionOperations: array("get", "post"),
-    itemOperations: array(
-        "get" => array(
-            "normalization_context" => array("groups" => array("cheese_listing:read", "cheese_listing:item:get") )
-        ),
-        "put", "delete", "patch"),
-    shortName: "cheeses",
-    attributes: array(
-        "pagination_items_per_page" => 5,
-        "formats" => array("jsonld", "json", "html", "jsonhal", "csv" => array("text/csv"))
-    ),
-    denormalizationContext: array("groups" => array("cheese_listing:write"), "swagger_definition_name" => "Write"),
-    normalizationContext: array("groups" => array("cheese_listing:read"), "swagger_definition_name" => "Read")
-)]
-#[ApiFilter(BooleanFilter::class, properties: array("isPublished"))]
-#[ApiFilter(SearchFilter::class, properties: array(
-    "title" => "partial",
-    "description" => "partial",
-    "owner" => "exact",
-    "owner.username" => "partial"
-))]
-#[ApiFilter(RangeFilter::class, properties: array("price"))]
-#[ApiFilter(PropertyFilter::class)]
 class CheeseListing
 {
     /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"cheese_listing:read", "cheese_listing:write", "user:read", "user:write"})
+     * @Assert\NotBlank()
+     * @Assert\Length(
+     *     min=2,
+     *     max=50,
+     *     maxMessage="Describe your cheese in 50 chars or less"
+     * )
      */
-    #[Groups(array("cheese_listing:read", "cheese_listing:write", "user:read", "user:write"))]
-    #[Assert\NotBlank()]
-    #[Assert\Length(array("min" => 2, "max" => 50, "maxMessage" => "Describe your cheese in 50 chars or less"))]
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Groups({"cheese_listing:read"})
+     * @Assert\NotBlank()
      */
-    #[Groups(array("cheese_listing:read"))]
-    #[Assert\NotBlank()]
     private $description;
 
     /**
+     * The price of this delicious cheese, in cents
+     *
      * @ORM\Column(type="integer")
+     * @Groups({"cheese_listing:read", "cheese_listing:write", "user:read", "user:write"})
+     * @Assert\NotBlank()
      */
-    #[Groups(array("cheese_listing:read", "cheese_listing:write", "user:read", "user:write"))]
-    #[Assert\NotBlank()]
-    #[Assert\Range(array("min" => 5, "max" => 50))]
     private $price;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
 
     /**
      * @ORM\Column(type="boolean")
@@ -81,24 +90,18 @@ class CheeseListing
     private $isPublished = false;
 
     /**
-     * @ORM\Column(type="datetime_immutable")
-     */
-    private $createdAt;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="cheeseListings")
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="cheeseListings")
      * @ORM\JoinColumn(nullable=false)
+     * @Groups({"cheese_listing:read", "cheese_listing:write"})
+     * @Assert\Valid()
      */
-    #[Groups(array("cheese_listing:read", "cheese_listing:write"))]
-    #[Assert\Valid]
     private $owner;
 
     public function __construct(string $title = null)
     {
-        $this->createdAt = new DateTimeImmutable();
         $this->title = $title;
+        $this->createdAt = new \DateTimeImmutable();
     }
-
 
     public function getId(): ?int
     {
@@ -115,26 +118,31 @@ class CheeseListing
         return $this->description;
     }
 
-    public function setDescription($description): void
-    {
-        $this->description = $description;
-    }
-
-    #[Groups(array("cheese_listing:read"))]
+    /**
+     * @Groups("cheese_listing:read")
+     */
     public function getShortDescription(): ?string
     {
         if (strlen($this->description) < 40) {
             return $this->description;
         }
 
-        return substr($this->description, 0, 40) . '...';
+        return substr($this->description, 0, 40).'...';
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
     }
 
     /**
-     * The description of the cheese as raw text
+     * The description of the cheese as raw text.
+     *
+     * @Groups({"cheese_listing:write", "user:write"})
+     * @SerializedName("description")
      */
-    #[Groups(array("cheese_listing:write", "user:write"))]
-    #[SerializedName("description")]
     public function setTextDescription(string $description): self
     {
         $this->description = nl2br($description);
@@ -154,6 +162,21 @@ class CheeseListing
         return $this;
     }
 
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * How long ago in text that this cheese listing was added.
+     *
+     * @Groups("cheese_listing:read")
+     */
+    public function getCreatedAtAgo(): string
+    {
+        return Carbon::instance($this->getCreatedAt())->diffForHumans();
+    }
+
     public function getIsPublished(): ?bool
     {
         return $this->isPublished;
@@ -164,20 +187,6 @@ class CheeseListing
         $this->isPublished = $isPublished;
 
         return $this;
-    }
-
-    /**
-     * How long ago in text that this cheese listing was added.
-     */
-    #[Groups(array("cheese_listing:read"))]
-    public function getCreatedAtAgo(): string
-    {
-        return Carbon::instance($this->getCreatedAt())->diffForHumans();
-    }
-
-    public function getCreatedAt(): ?DateTimeImmutable
-    {
-        return $this->createdAt;
     }
 
     public function getOwner(): ?User
--- application/src/Entity/User.php
+++ application/src/Entity/User.php
@@ -3,44 +3,41 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiFilter;
-use App\Repository\UserRepository;
-use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Core\Annotation\ApiResource;
+use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
-use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
 
 /**
- * @ORM\Entity(repositoryClass=UserRepository::class)
- * @ORM\Table(name="`user`")
+ * @ApiResource(
+ *     normalizationContext={"groups"={"user:read"}},
+ *     denormalizationContext={"groups"={"user:write"}},
+ * )
+ * @ApiFilter(PropertyFilter::class)
+ * @UniqueEntity(fields={"username"})
+ * @UniqueEntity(fields={"email"})
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-#[ApiResource(
-    denormalizationContext: array("groups" => array("user:write")),
-    normalizationContext: array("groups" => array("user:read"))
-)]
-#[ApiFilter(PropertyFilter::class)]
-#[UniqueEntity(fields: array("username"))]
-#[UniqueEntity(fields: array("email"))]
-class User implements UserInterface, PasswordAuthenticatedUserInterface
+class User implements UserInterface
 {
     /**
-     * @ORM\Id
-     * @ORM\GeneratedValue
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Groups({"user:read", "user:write"})
+     * @Assert\NotBlank()
+     * @Assert\Email()
      */
-    #[Groups(array("user:read", "user:write"))]
-    #[Assert\NotBlank]
-    #[Assert\Email]
     private $email;
 
     /**
@@ -51,22 +48,22 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @var string The hashed password
      * @ORM\Column(type="string")
+     * @Groups({"user:write"})
      */
-    #[Groups(array("user:write"))]
     private $password;
 
     /**
      * @ORM\Column(type="string", length=255, unique=true)
+     * @Groups({"user:read", "user:write", "cheese_listing:item:get", "cheese_listing:write"})
+     * @Assert\NotBlank()
      */
-    #[Groups(array("user:read", "user:write", "cheese_listing:item:get", "cheese_listing:write"))]
-    #[Assert\NotBlank]
     private $username;
 
     /**
-     * @ORM\OneToMany(targetEntity=CheeseListing::class, mappedBy="owner", cascade={"persist"}, orphanRemoval=true)
+     * @ORM\OneToMany(targetEntity="App\Entity\CheeseListing", mappedBy="owner", cascade={"persist"}, orphanRemoval=true)
+     * @Groups({"user:read", "user:write"})
+     * @Assert\Valid()
      */
-    #[Groups(array("user:read", "user:write"))]
-    #[Assert\Valid]
     private $cheeseListings;
 
     public function __construct()
@@ -96,21 +93,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      *
      * @see UserInterface
      */
-    public function getUserIdentifier(): string
-    {
-        return (string)$this->email;
-    }
-
     public function getUsername(): string
     {
-        return $this->username;
-    }
-
-    public function setUsername(string $username): self
-    {
-        $this->username = $username;
-
-        return $this;
+        return (string) $this->username;
     }
 
     /**
@@ -133,11 +118,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     }
 
     /**
-     * @see PasswordAuthenticatedUserInterface
+     * @see UserInterface
      */
     public function getPassword(): string
     {
-        return $this->password;
+        return (string) $this->password;
     }
 
     public function setPassword(string $password): self
@@ -148,14 +133,11 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     }
 
     /**
-     * Returning a salt is only needed, if you are not using a modern
-     * hashing algorithm (e.g. bcrypt or sodium) in your security.yaml.
-     *
      * @see UserInterface
      */
-    public function getSalt(): ?string
+    public function getSalt()
     {
-        return null;
+        // not needed when using the "bcrypt" algorithm in security.yaml
     }
 
     /**
@@ -165,6 +147,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
     }
 
     /**
@@ -187,7 +176,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
     public function removeCheeseListing(CheeseListing $cheeseListing): self
     {
-        if ($this->cheeseListings->removeElement($cheeseListing)) {
+        if ($this->cheeseListings->contains($cheeseListing)) {
+            $this->cheeseListings->removeElement($cheeseListing);
             // set the owning side to null (unless already changed)
             if ($cheeseListing->getOwner() === $this) {
                 $cheeseListing->setOwner(null);
