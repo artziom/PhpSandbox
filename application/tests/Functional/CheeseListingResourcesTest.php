e5739da3eb562d343818e2bdab83ff56eaec0e19
ArtZiom <artziom@gmail.com>
2021-12-02 13:50:18 +0100

    Crate first test

---
+++ application/tests/Functional/CheeseListingResourcesTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Functional;
+
+use App\ApiPlatform\Test\ApiTestCase;
+
+class CheeseListingResourcesTest extends ApiTestCase
+{
+    public function testCreateCheeseListing()
+    {
+        $client = self::createClient();
+
+        $client->request("POST", '/api/cheeses', [
+            'headers' => ['Content-Type' => 'application/json'],
+            'json' => []
+        ]);
+        $this->assertResponseStatusCodeSame(401);
+    }
+}
