8aa2908f8a70bc74b97b3b9bdc8b554e2268026d
ArtZiom <artziom@gmail.com>
2021-12-12 22:37:26 +0100

    Create context builder

--- application/tests/Functional/UserResourceTest.php
+++ application/tests/Functional/UserResourceTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional;
 
+use App\Entity\User;
 use App\Test\CustomApiTestCase;
 use Hautelook\AliceBundle\PhpUnit\ReloadDatabaseTrait;
 
@@ -57,5 +58,15 @@ class UserResourceTest extends CustomApiTestCase
 
         $data = $client->getResponse()->toArray();
         $this->assertArrayNotHasKey("phoneNumber", $data);
+
+        $user = $em->getRepository(User::class)->find($user->getId());
+        $user->setRoles(['ROLE_ADMIN']);
+        $em->flush();
+        $this->logIn($client, 'admin@foo.pl', 'foo');
+
+        $client->request('GET', '/api/users/'.$user->getId());
+        $this->assertJsonContains([
+            'phoneNumber' => '555.111.222'
+        ]);
     }
 }
