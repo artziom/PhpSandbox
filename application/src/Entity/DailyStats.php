8eb0731d6528de7eeca86156d249df3dc635eb3b
ArtZiom <artziom@gmail.com>
2022-01-01 22:01:00 +0100

    Create custom resource and set path segment name generator to ...dash

---
+++ application/src/Entity/DailyStats.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Entity;
+
+
+use ApiPlatform\Core\Annotation\ApiResource;
+
+/**
+ * @ApiResource
+ */
+class DailyStats
+{
+    public $date;
+
+    public $totalVisitors;
+
+    public $mostPopularListings;
+}
