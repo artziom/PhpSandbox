0c3d3c120f9b680c832b984914996571a5efcdca
ArtZiom <artziom@gmail.com>
2022-01-08 22:20:10 +0100

    Remove old project

--- application/assets/js/app.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import CheeseWhizApp from './components/CheeseWhizApp';
-import 'bootstrap/dist/css/bootstrap.css';
-
-new Vue({
-    render(h) {
-        return h(CheeseWhizApp);
-    },
-}).$mount('#cheese-app');
