44f5c379cd6ac0e6588700634ac2ddf296641382
ArtZiom <artziom@gmail.com>
2022-01-11 14:42:18 +0100

    Create homepage controller

---
+++ application/src/Controller/QuestionController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController
+{
+    #[Route("/")]
+    public function homepage(){
+        return new Response('Hello World!');
+    }
+}
