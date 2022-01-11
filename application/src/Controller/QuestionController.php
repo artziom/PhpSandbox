1f166c0636bc56b187b8427268149bbac3d3e437
ArtZiom <artziom@gmail.com>
2022-01-11 14:58:50 +0100

    Add new route

--- application/src/Controller/QuestionController.php
+++ application/src/Controller/QuestionController.php
@@ -12,4 +12,12 @@ class QuestionController
     {
         return new Response('Hello World!');
     }
+
+    #[Route("/questions/{slug}")]
+    public function show($slug): Response
+    {
+        return new Response(sprintf("Future page to show a question \"%s\"!",
+            ucwords(str_replace("-", " ", $slug))
+        ));
+    }
 }
