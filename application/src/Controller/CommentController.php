6b86673976d1a38c908202decfab95172e90e47d
ArtZiom <artziom@gmail.com>
2022-01-12 13:00:22 +0100

    Create comment vote route

---
+++ application/src/Controller/CommentController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentController extends AbstractController
+{
+    #[Route("/comments/{id}/vote/{direction}", name: "")]
+    public function commentVote($id, $direction): Response
+    {
+        if ($direction === 'up') {
+            $currentVoteCount = rand(7, 100);
+        } else {
+            $currentVoteCount = rand(0, 5);
+        }
+
+        return $this->json(['votes' => $currentVoteCount]);
+    }
+}
