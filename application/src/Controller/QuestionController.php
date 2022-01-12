608b9d09308bd73dcdc814a61675dc9172e5f331
ArtZiom <artziom@gmail.com>
2022-01-12 12:30:55 +0100

    Install profiler

--- application/src/Controller/QuestionController.php
+++ application/src/Controller/QuestionController.php
@@ -23,6 +23,8 @@ class QuestionController extends AbstractController
             "But why?"
         ];
 
+        dump($slug, $this);
+
         return $this->render("question/show.html.twig", [
             'question' => ucwords(str_replace("-", " ", $slug)),
             'answers' => $answers
