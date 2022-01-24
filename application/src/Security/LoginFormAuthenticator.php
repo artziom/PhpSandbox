3cbdf63c1368c3e48570cf2c0ea0a8b60d0392ea
ArtZiom <artziom@gmail.com>
2022-01-24 14:15:03 +0100

    Create authentication process

--- application/src/Security/LoginFormAuthenticator.php
+++ application/src/Security/LoginFormAuthenticator.php
@@ -2,33 +2,74 @@
 
 namespace App\Security;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UserNotFoundException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
+use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
+use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Authenticator\Passport\PassportInterface;
 
 class LoginFormAuthenticator extends AbstractAuthenticator
 {
+    private UserRepository $userRepository;
+    private RouterInterface $router;
+
+    public function __construct(UserRepository $userRepository, RouterInterface $router)
+    {
+        $this->userRepository = $userRepository;
+        $this->router = $router;
+    }
+
     public function supports(Request $request): ?bool
     {
-        dd('supports');
+        return $request->getPathInfo() === '/login' && $request->isMethod('POST');
     }
 
     public function authenticate(Request $request): PassportInterface
     {
-        // TODO: Implement authenticate() method.
+        $email = $request->request->get('email');
+        $password = $request->request->get('password');
+
+
+        return new Passport(
+            new UserBadge($email, function ($userIdentifier){
+                $user = $this->userRepository->findOneBy(['email' => $userIdentifier]);
+
+                if(!$user){
+                    throw new UserNotFoundException();
+                }
+
+                return $user;
+            }),
+            new CustomCredentials(function ($credentials, User $user){
+               return $credentials === 'foo';
+            }, $password)
+        );
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $firewallName): ?Response
     {
-        // TODO: Implement onAuthenticationSuccess() method.
+        return new RedirectResponse(
+            $this->router->generate('app_homepage')
+        );
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
-        // TODO: Implement onAuthenticationFailure() method.
+        $request->getSession()->set(Security::AUTHENTICATION_ERROR, $exception);
+
+        return new RedirectResponse(
+            $this->router->generate('app_login')
+        );
     }
 
 //    public function start(Request $request, AuthenticationException $authException = null): Response
