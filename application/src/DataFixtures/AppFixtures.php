f21542f653266ded9052840520cd39a7a624e517
ArtZiom <artziom@gmail.com>
2022-01-26 15:46:13 +0100

    Add owner field to questions

--- application/src/DataFixtures/AppFixtures.php
+++ application/src/DataFixtures/AppFixtures.php
@@ -14,9 +14,22 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        UserFactory::createOne([
+            'email' => 'admin@foo.com',
+            'roles' => ['ROLE_ADMIN']
+        ]);
+        UserFactory::createOne([
+            'email' => 'user@foo.com'
+        ]);
+        UserFactory::createMany(10);
+
         TagFactory::createMany(100);
 
-        $questions = QuestionFactory::createMany(20);
+        $questions = QuestionFactory::createMany(20, function (){
+            return [
+              'owner' => UserFactory::random()
+            ];
+        });
 
         QuestionTagFactory::createMany(100, function () {
             return [
@@ -40,15 +53,6 @@ class AppFixtures extends Fixture
                 'question' => $questions[array_rand($questions)]
             ];
         })->needsApproval()->many(20)->create();
-
-        UserFactory::createOne([
-            'email' => 'admin@foo.com',
-            'roles' => ['ROLE_ADMIN']
-        ]);
-        UserFactory::createOne([
-            'email' => 'user@foo.com'
-        ]);
-        UserFactory::createMany(10);
 
         $manager->flush();
     }
