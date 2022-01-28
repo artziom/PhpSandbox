7c10e163071aefa3a43bcdd06e2fc3485f462420
ArtZiom <artziom@gmail.com>
2022-01-28 15:55:45 +0100

    Add state and refactor code

--- application/assets/controllers/color-square_controller.js
+++ application/assets/controllers/color-square_controller.js
@@ -1,6 +1,7 @@
 import {Controller} from "@hotwired/stimulus";
 
 export default class extends Controller{
+    selectedColorId = null;
     static targets = ['colorSquare', 'select'];
 
     connect() {
@@ -8,10 +9,33 @@ export default class extends Controller{
     }
 
     selectColor(event) {
+        this.setSelectedColor(event.currentTarget.dataset.colorId)
+    }
+
+    setSelectedColor(clickedColorId){
+        if(clickedColorId === this.selectedColorId){
+            this.findSelectedColorSquare().classList.remove('selected');
+            this.selectedColorId = null;
+            this.selectTarget.value = '';
+
+            return;
+        }
+
+        this.selectedColorId = clickedColorId;
+
         this.colorSquareTargets.forEach((element) => {
            element.classList.remove('selected');
         });
-        event.currentTarget.classList.add('selected');
-        this.selectTarget.value = event.currentTarget.dataset.colorId
+
+        this.findSelectedColorSquare().classList.add('selected');
+        this.selectTarget.value = this.selectedColorId;
+    }
+
+    /**
+     *
+     * @returns {Element|null}
+     */
+    findSelectedColorSquare(){
+        return this.colorSquareTargets.find((element) => element.dataset.colorId === this.selectedColorId);
     }
 }
