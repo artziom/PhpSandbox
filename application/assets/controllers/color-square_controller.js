01ba638dc2188e4a7f575a17c23584b00f5fd32a
ArtZiom <artziom@gmail.com>
2022-01-28 15:32:51 +0100

    Remove selected class from others buttons after selecting one color

--- application/assets/controllers/color-square_controller.js
+++ application/assets/controllers/color-square_controller.js
@@ -1,7 +1,11 @@
 import {Controller} from "@hotwired/stimulus";
 
 export default class extends Controller{
+    static targets = ['colorSquare'];
     selectColor(event) {
-        console.log(event);
+        this.colorSquareTargets.forEach((element) => {
+           element.classList.remove('selected');
+        });
+        event.currentTarget.classList.add('selected');
     }
 }
