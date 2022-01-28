9e81313d702506cf8e088dda5a7ec61c8d7de83f
ArtZiom <artziom@gmail.com>
2022-01-28 23:09:21 +0100

    Code refactor - use on value change callback

--- application/assets/controllers/color-square_controller.js
+++ application/assets/controllers/color-square_controller.js
@@ -1,45 +1,29 @@
 import {Controller} from "@hotwired/stimulus";
 
-export default class extends Controller{
-    selectedColorId = null;
+export default class extends Controller {
     static targets = ['colorSquare', 'select'];
     static values = {
-      colorId: Number
+        colorId: Number
     };
 
     connect() {
         this.selectTarget.classList.add('d-none');
-        console.log(this.colorIdValue);
     }
 
     selectColor(event) {
-        this.setSelectedColor(event.currentTarget.dataset.colorId)
+        const clickedColor = event.currentTarget.dataset.colorId;
+        this.colorIdValue = clickedColor == this.colorIdValue ? null : clickedColor;
     }
 
-    setSelectedColor(clickedColorId){
-        if(clickedColorId === this.selectedColorId){
-            this.findSelectedColorSquare().classList.remove('selected');
-            this.selectedColorId = null;
-            this.selectTarget.value = '';
-
-            return;
-        }
-
-        this.selectedColorId = clickedColorId;
+    colorIdValueChanged() {
+        this.selectTarget.value = this.colorIdValue;
 
         this.colorSquareTargets.forEach((element) => {
-           element.classList.remove('selected');
+            if(element.dataset.colorId == this.colorIdValue){
+                element.classList.add('selected');
+            }else{
+                element.classList.remove('selected');
+            }
         });
-
-        this.findSelectedColorSquare().classList.add('selected');
-        this.selectTarget.value = this.selectedColorId;
-    }
-
-    /**
-     *
-     * @returns {Element|null}
-     */
-    findSelectedColorSquare(){
-        return this.colorSquareTargets.find((element) => element.dataset.colorId === this.selectedColorId);
     }
 }
