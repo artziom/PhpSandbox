67c51d280180d36ae82ef4c491cb788f7421a247
ArtZiom <artziom@gmail.com>
2022-01-28 12:54:02 +0100

    Create counter stimulus controller

---
+++ application/assets/controllers/counter_controller.js
@@ -0,0 +1,13 @@
+import {Controller} from "@hotwired/stimulus";
+
+export default class extends Controller{
+    connect() {
+        this.element.innerHTML = "You have clicked me 0 times :(";
+        this.count = 0;
+
+        this.element.addEventListener('click', () => {
+           this.count++;
+           this.element.innerHTML = this.count;
+        });
+    }
+}
