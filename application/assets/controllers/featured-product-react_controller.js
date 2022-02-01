d8633d2ae2c5fb8b9aafc5a857aded3164cc9e2f
ArtZiom <artziom@gmail.com>
2022-02-01 23:06:31 +0100

    Passing data to react component

---
+++ application/assets/controllers/featured-product-react_controller.js
@@ -0,0 +1,16 @@
+import {Controller} from "@hotwired/stimulus";
+import ReactDOM from 'react-dom';
+import React from 'react';
+import FeaturedProduct from "../components/FeaturedProduct";
+
+export default class extends Controller{
+    static values = {
+        product: Object
+    }
+    connect() {
+        ReactDOM.render(
+            <FeaturedProduct product={this.productValue} />,
+            this.element
+        )
+    }
+}
