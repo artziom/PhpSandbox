34c8b85f1af9b83f297720dab33974bc711abb73
ArtZiom <artziom@gmail.com>
2022-02-01 15:40:00 +0100

    Listen to the new event, and create partial template for shopping cart list

--- application/assets/controllers/cart-list_controller.js
+++ application/assets/controllers/cart-list_controller.js
@@ -2,7 +2,7 @@ import {Controller} from "@hotwired/stimulus";
 import Swal from "sweetalert2";
 
 export default class extends Controller{
-    connect() {
-        console.log('cart');
+    removeItem(event) {
+        console.log(event.currentTarget);
     }
 }
